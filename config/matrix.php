db318f641488fd29f97e24df50e7faf8c024ec0c
wangdong <wangdonghb@163.com>
2016-09-16 10:50:24 +0800

    优化

--- config/matrix.php
+++ config/matrix.php
@@ -2,12 +2,10 @@
 
 return [
 
-    'name' => 'MATRIX',
-    'header' => [
-        'logo' => [
-            'mini' => '<b>M</b>',
-            'normal' => '<b>MATRIX</b>',
-        ],
+    'name' => env('MATRIX_NAME', 'MATRIX'),
+    'logo' => [
+        'mini' => env('MATRIX_LOGO_MINI', '<b>M</b>'),
+        'normal' => env('MATRIX_LOGO', '<b>MATRIX</b>'),
     ],
 
 ];
