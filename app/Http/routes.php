d5880eb3da9c38f4ea3c698bb1fb7509cb68b8d4
wangdong <wangdonghb@163.com>
2016-09-16 11:23:40 +0800

    登录验证方式调整

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,33 +15,30 @@ Route::get('/', function () {
     return view('index');
 });
 
-// 插件模块
-//Route::group(['middleware' => 'auth'], function () {
+// 插件模块(登录验证需要插件自己调用auth中间件)
+$path = app('request')->path();
+$route = explode('/', $path);
 
-    $path = app('request')->path();
-    $route = explode('/', $path);
+if (count($route) >= 2) {
+    $group = $route[0];
+    $name = $route[1];
 
-    if (count($route) >= 2) {
-        $group = $route[0];
-        $name = $route[1];
+    $controller = '\\Matrix\\' . studly_case(strtolower($group)) . '\\' . studly_case(strtolower($name)) . '\\Controller';
+    if (class_exists($controller)) {
+        // 反解析类
+        $object = new \ReflectionClass($controller);
+        $pluginPath = dirname(dirname($object->getFileName())); //插件根目录
 
-        $controller = '\\Matrix\\' . studly_case(strtolower($group)) . '\\' . studly_case(strtolower($name)) . '\\Controller';
-        if (class_exists($controller)) {
-            // 反解析类
-            $object = new \ReflectionClass($controller);
-            $pluginPath = dirname(dirname($object->getFileName())); //插件根目录
+        // 设置插件模板根目录
+        config([
+            'view.paths' => [
+                realpath($pluginPath . '/views')
+            ],
+        ]);
 
-            // 设置插件模板根目录
-            config([
-                'view.paths' => [
-                    realpath($pluginPath . '/views')
-                ],
-            ]);
-
-            Route::resource("{$group}/{$name}", $controller);
-        }
+        Route::resource("{$group}/{$name}", $controller);
     }
-//});
+}
 
 // 系统核心模块 (所有路由都可以使用插件进行覆盖)
 Route::controller('matrix', 'MatrixController');
