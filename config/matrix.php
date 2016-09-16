c48fbe400ed197142f59db2135dcee98780b5f94
wangdong <wangdonghb@163.com>
2016-09-16 11:14:03 +0800

    优化

--- config/matrix.php
+++ config/matrix.php
@@ -3,9 +3,12 @@
 return [
 
     'name' => env('MATRIX_NAME', 'MATRIX'),
+
     'logo' => [
         'mini' => env('MATRIX_LOGO_MINI', '<b>M</b>'),
         'normal' => env('MATRIX_LOGO', '<b>MATRIX</b>'),
     ],
+
+    'version' => env('MATRIX_VERSION', '1.0.0'),
 
 ];
