85d1bb0b9eee8cc37c83b32c8b0d84ba8a347c7a
wangdong <wangdong1@kuaiqiangche.com>
2016-10-10 13:52:18 +0800

    优化

--- app/Http/Controllers/MatrixController.php
+++ app/Http/Controllers/MatrixController.php
@@ -54,7 +54,7 @@ class MatrixController extends Controller
         $pluginFiles = $filesystem->glob(base_path('matrix/*/{composer.json,*/composer.json,*/*/composer.json}'), GLOB_BRACE);
         collect($pluginFiles)->each(function ($file) use (&$installed, $filesystem) {
             $plugin = json_decode($filesystem->get($file), true);
-            if(!array_get($plugin, 'time')) array_set($plugin, 'time', date('Y-m-d H:i:s'));
+            if (!array_get($plugin, 'time')) array_set($plugin, 'time', date('Y-m-d H:i:s'));
             array_push($installed, $plugin);
         });
 
@@ -89,15 +89,17 @@ class MatrixController extends Controller
         });
 
         $menus = $collect->map(function ($item) use ($groups) {
-            // 菜单路由根据命名空间生成
-            $namespace = trim(collect(array_get($item, 'autoload.psr-4', []))->keys()->first(), '\\');
-            $namespace = explode('\\', $namespace);
-            $url = count($namespace) > 2 ? snake_case($namespace[1]) . '/' . snake_case($namespace[2]) : '#';
-
             // 生成链接
-            array_set($item, 'extra.plugin.menu.url', $url);
+            if (count(array_get($item, 'extra.plugin.menu.children', [])) == 0 && !array_get($item, 'extra.plugin.menu.url')) {
+                // 菜单路由根据命名空间生成
+                $namespace = trim(collect(array_get($item, 'autoload.psr-4', []))->keys()->first(), '\\');
+                $namespace = explode('\\', $namespace);
+                $url = count($namespace) > 2 ? snake_case($namespace[1]) . '/' . snake_case($namespace[2]) : '#';
 
-            // 24小时内安装的菜单显示new标签
+                array_set($item, 'extra.plugin.menu.url', $url);
+            }
+
+            // 24小时内发布的插件菜单显示new标签
             if (strtotime($item['time'] . '+ 1 day') > time()) {
                 $new = [
                     'class' => 'bg-green',
